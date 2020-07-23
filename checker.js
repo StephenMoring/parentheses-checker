cd7d3e2f170a6720604a53e4e932d3bda478d674
Stephen Moring <stephenmoring@stephens-mbp.attlocal.net>
2020-07-22 20:15:29 -0400

    added working logic, parameterss, and return to checkParentheses

--- checker.js
+++ checker.js
@@ -1,3 +1,18 @@
 import { readFileSync } from "fs";
+
+export function checkParentheses(text){
+    let parenthesesStack = []; 
+    for(let c of text){
+        if (c === '('){
+            parenthesesStack.push('(');
+        } else if (c === ')'){
+            if(parenthesesStack.pop() === undefined){
+                return false;
+            }
+        }
+    }
+    return parenthesesStack.length == 0 ? true : false;
+}
+
 var text = readFileSync("./tests/test_lisp_code.txt").toString('utf-8');
-console.log(text);
+console.log(checkParentheses(text));
